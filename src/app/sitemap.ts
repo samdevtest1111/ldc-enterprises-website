792f4fe889eacf35d86633a5cc6a6b19cf9327ba
L <changethislater@gmail.com>
2026-04-04 20:36:35 +0530

    fix: update sitemap domain to ldcenterprises.in

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.ellicyenterprises.com", // <--- Put your actual domain here
+      url: "https://www.ldcenterprises.in", // <--- Put your actual domain here
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
